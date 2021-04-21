2297169cfb332b60e8259ecf19c76294609e5424
wb-786711 <wb-786711@alibaba-inc.com>
2021-04-21 10:15:09 +0800

    mq使用

--- mybatis/src/test/java/com/afei/mybatis/RabbitTests.java
+++ mybatis/src/test/java/com/afei/mybatis/RabbitTests.java
@@ -2,6 +2,7 @@ package com.afei.mybatis;
 
 import com.afei.mybatis.product.Sender;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,6 +13,10 @@ class RabbitTests {
 
 	@Autowired
 	Sender sender;
+
+	@Autowired
+	RabbitTemplate rabbitTemplate;
+
 	@Test
 	void send() {
 		IntStream.rangeClosed(0,20).forEach(item -> {
@@ -19,4 +24,17 @@ class RabbitTests {
 		});
 	}
 
+	@Test
+	public void send1() {
+		String context = "hi, i am message 1";
+		System.out.println("Sender : " + context);
+		this.rabbitTemplate.convertAndSend("exchange", "topic.message", context);
+	}
+
+	@Test
+	public void send2() {
+		String context = "hi, i am messages 2";
+		System.out.println("Sender : " + context);
+		this.rabbitTemplate.convertAndSend("exchange", "topic.messages", context);
+	}
 }
